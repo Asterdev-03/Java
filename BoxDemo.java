749a14291260a5518c60fe57ffc61ea73f13fe24
Aswin <aswinkarat@gmail.com>
2022-01-20 17:46:18 +0530

    Multiclass example

--- BoxDemo.java
+++ BoxDemo.java
@@ -1,16 +1,21 @@
+// A simple Multi Class Example
+
 import java.util.Scanner;
 
+// Declaration of class Box
 class Box {
     int length;
     int width;
     int height;
 
-    Box(int l, int w, int h) {
-        this.length = l;
-        this.width = w;
-        this.height = h;
+    // Initializing the dimensions of the Box object
+    Box(int length, int width, int height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
 
+    // Returning the volume of the Box object
     int volume() {
         return length * width * height;
     }
@@ -21,15 +26,17 @@ public class BoxDemo {
         int length, width, height, volume;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Dimensions:");
+        System.out.print("Enter Length: ");
         length = sc.nextInt();
+        System.out.print("Enter Width: ");
         width = sc.nextInt();
+        System.out.print("Enter Heigth: ");
         height = sc.nextInt();
 
         Box myBox = new Box(length, width, height);
 
         volume = myBox.volume();
 
-        System.out.println("Volume: " + volume);
+        System.out.println("Volume of the Box: " + volume);
     }
 }
