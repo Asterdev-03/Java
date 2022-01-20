d329db0c519fa8c43189230604867e4ab95021f4
Aswin <aswinkarat@gmail.com>
2022-01-20 17:43:53 +0530

    to display Hello World

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
-class hello 
-{
+// A simple Java progam to print Hello World
+
+class hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World");
     }
 }
