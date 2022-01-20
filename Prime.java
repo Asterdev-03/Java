500654a9e5dd48c00b7cbe6fffddd7072f22be29
Aswin <aswinkarat@gmail.com>
2022-01-20 17:44:08 +0530

    to check Prime or not

--- Prime.java
+++ Prime.java
@@ -1,20 +1,24 @@
+// Program to check whether a number is Prime or not
+
 import java.util.Scanner;
+
 public class Prime {
     public static void main(String[] args) {
-        int num, flag = 0;
+        int num;
+        boolean flag = false;
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter the number: ");
             num = sc.nextInt();
         }
-        if(num <= 1)
-            flag = 1;
+        if (num <= 1)
+            flag = true;
         else
-            for(int i=2; i <= num/2; i++)
-                if(num%2 == 0){
-                    flag = 1;
+            for (int i = 2; i <= num / 2; i++)
+                if (num % 2 == 0) {
+                    flag = true;
                     break;
                 }
-        if(flag == 0)
+        if (flag)
             System.out.println("Prime");
         else
             System.out.println("Not Prime");
